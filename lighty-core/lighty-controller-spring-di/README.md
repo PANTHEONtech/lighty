830f7381bf51769a9c9539fa79707e679bbd0fa1
Michal Banik <vainyksi@users.noreply.github.com>
2018-06-13 09:37:36 +0200

    lighty.io and springboot integration (#4)
    * lighty.io dependency injection provider for spring.io
    
      add lighty-core preconfigured @Configuration class for initializing spring beans
    
      since now the spring developers need only to:
        1. extend LightyCoreSpringConfiguration class with @Configuration annotation
        2. init LightyController as bean (and other needed lighty modules)
        3. use lighty services as spring beans
    
    Signed-off-by: Michal Banik <banik.michal.sk@gmail.com>
    
    * simple example of lighty & netconf integration into springboot application
    
      adds very basic netconf functionality to existing springboot application exposed to REST endpoints:
        GET /topology/list
          - list all topology IDs stored in datastore
        PUT /topology/id/{topologyId}
          - create new topology with topology id "test-topology-id"
        DELETE /topology/id/{topologyId}
          - delete existing topology with topology id "test-topology-id"
        GET /netconf/list
          - list all NetConf devices with its connection status and "darknessFactor" data loaded from device (darknessFactor is contained in toaster model from ODL)
        PUT /netconf/id/{netconfDeviceId}
          - attempt to connect to device "test-device" with specific credentials and address:port
        DELETE /netconf/id/{netconfDeviceId}
          - disconnect NetConf device "test-device"
    
    Signed-off-by: Michal Banik <banik.michal.sk@gmail.com>

---
+++ lighty-core/lighty-controller-spring-di/README.md
@@ -0,0 +1,72 @@
+Lighty Spring.io Dependency Injection Extension
+===============================================
+This project provides Lighty Core module for
+[Spring.io](https://spring.io/)
+Dependency Injection Extension. This extension is available for Lighty projects
+application projects using [spring.io](https://spring.io/) as runtime environment.
+
+```
+io.lighty.core.controller.spring.LightyCoreSpringConfiguration
+```
+
+How to use it
+-------------
+1. Add dependency into your project
+```
+   <dependency>
+      <groupId>io.lighty.core</groupId>
+      <artifactId>lighty-controller-spring-di</artifactId>
+      <version>${lighty.version}</version>
+   </dependency>
+```
+
+2. Extend ```LightyCoreSpringConfiguration``` class to apply lighty.io services bean configuration and 
+initialize ```LightyConttroller``` as spring bean with whichever custom configuration you want
+```
+@Configuration
+public class LightyConfiguration extends LightyCoreSpringConfiguration {
+
+    @Bean
+    LightyController initLightyController() throws Exception {
+        LOG.info("Building LightyController Core");
+        final LightyControllerBuilder lightyControllerBuilder = new LightyControllerBuilder();
+        final Set<YangModuleInfo> mavenModelPaths = new HashSet<>();
+        mavenModelPaths.addAll(NetconfConfigUtils.NETCONF_TOPOLOGY_MODELS);
+        LightyController lightyController = lightyControllerBuilder
+            .from(ControllerConfigUtils.getDefaultSingleNodeConfiguration(mavenModelPaths))
+            .build();
+        LOG.info("Starting LightyController (waiting 10s after start)");
+        final ListenableFuture<Boolean> started = lightyController.start();
+        started.get();
+        LOG.info("LightyController Core started");
+    
+        Runtime.getRuntime().addShutdownHook(new LightyModuleShutdownHook(lightyController));
+    
+        return lightyController;
+    }
+    ...
+}
+```
+
+3. Inject & use ODL services in your code
+```
+@RestController
+@RequestMapping(path = "netconf")
+public class NetconfDeviceRestService {
+
+    private static final InstanceIdentifier<Topology> NETCONF_TOPOLOGY_IID = InstanceIdentifier
+        .create(NetworkTopology.class)
+        .child(Topology.class, new TopologyKey(new TopologyId("topology-netconf")));
+    private static final long TIMEOUT = 1;
+    private static final InstanceIdentifier<Toaster> TOASTER_IID = InstanceIdentifier.create(Toaster.class);
+
+    @Autowired
+    @Qualifier("BindingDataBroker")
+    private DataBroker dataBroker;
+
+    @Autowired
+    private MountPointService mountPointService;
+
+    ...
+}
+```
---
+++ lighty-core/lighty-controller-spring-di/src/main/java/io/lighty/core/controller/spring/LightyCoreSpringConfiguration.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the lighty.io-core
+ * Fair License 5, version 0.9.1. You may obtain a copy of the License
+ * at: https://github.com/PantheonTechnologies/lighty-core/LICENSE.md
+ */
+package io.lighty.core.controller.spring;
+
+import io.lighty.core.controller.api.LightyController;
+import io.netty.channel.EventLoopGroup;
+import io.netty.util.Timer;
+import io.netty.util.concurrent.EventExecutor;
+import org.opendaylight.controller.cluster.ActorSystemProvider;
+import org.opendaylight.controller.cluster.datastore.DistributedDataStoreInterface;
+import org.opendaylight.controller.cluster.sharding.DistributedShardFactory;
+import org.opendaylight.controller.config.threadpool.ScheduledThreadPool;
+import org.opendaylight.controller.config.threadpool.ThreadPool;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.MountPointService;
+import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
+import org.opendaylight.controller.md.sal.binding.api.NotificationService;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
+import org.opendaylight.controller.md.sal.dom.api.DOMNotificationPublishService;
+import org.opendaylight.controller.md.sal.dom.api.DOMNotificationService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
+import org.opendaylight.controller.md.sal.dom.clustering.impl.LegacyEntityOwnershipServiceAdapter;
+import org.opendaylight.controller.md.sal.dom.spi.DOMNotificationSubscriptionListenerRegistry;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.controller.sal.core.api.model.YangTextSourceProvider;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTreeFactory;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeService;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeShardingService;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
+import org.opendaylight.mdsal.dom.api.DOMYangTextSourceProvider;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
+import org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.ClusterAdminService;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Base lighty.io Configuration class for spring DI.
+ * <p>
+ * This configuration needs LightyController bean initialized in spring environment. If LightyController bean exists in
+ * spring environment, this configuration initializes all core lighty.io services as spring beans.</p>
+ * <p>
+ * Example:
+ * <pre>
+ * &#64;Configuration
+ * public class LightyConfiguration extends LightyCoreSprigConfiguration {
+ *     &#64;Bean
+ *     LightyController initLightyController() throws Exception {
+ *
+ *         LightyController lightyController = ...
+ *
+ *         return lightyController;
+ *     }
+ * }
+ * </pre>
+ *
+ * @author juraj.veverka
+ */
+@Configuration
+public class LightyCoreSpringConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LightyCoreSpringConfiguration.class);
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private LightyController lightyController;
+
+    @Bean
+    public ActorSystemProvider getActorSystemProvider() {
+        return lightyController.getServices().getActorSystemProvider();
+    }
+
+    @Bean
+    public SchemaContextProvider getSchemaContextProvider() {
+        return lightyController.getServices().getSchemaContextProvider();
+    }
+
+    @Bean
+    public DOMSchemaService getDOMSchemaService() {
+        return lightyController.getServices().getDOMSchemaService();
+    }
+
+    @Bean
+    public DOMYangTextSourceProvider getDOMYangTextSourceProvider() {
+        return lightyController.getServices().getDOMYangTextSourceProvider();
+    }
+
+    @Bean
+    public DOMMountPointService getDOMMountPointService() {
+        return lightyController.getServices().getDOMMountPointService();
+    }
+
+    @Bean
+    public DOMNotificationPublishService getDOMNotificationPublishService() {
+        return lightyController.getServices().getDOMNotificationPublishService();
+    }
+
+    @Bean
+    public DOMNotificationService getDOMNotificationService() {
+        return lightyController.getServices().getDOMNotificationService();
+    }
+
+    @Bean
+    public DOMNotificationSubscriptionListenerRegistry getDOMNotificationSubscriptionListenerRegistry() {
+        return lightyController.getServices().getDOMNotificationSubscriptionListenerRegistry();
+    }
+
+    @Bean(name = "ConfigDatastore")
+    public DistributedDataStoreInterface getConfigDatastore() {
+        return lightyController.getServices().getConfigDatastore();
+    }
+
+    @Bean(name = "OperationalDatastore")
+    public DistributedDataStoreInterface getOperationalDatastore() {
+        return lightyController.getServices().getOperationalDatastore();
+    }
+
+    @Bean(name = "ClusteredDOMDataBroker")
+    public DOMDataBroker getClusteredDOMDataBroker() {
+        return lightyController.getServices().getClusteredDOMDataBroker();
+    }
+
+    @Bean(name = "PingPongDataBroker")
+    public DOMDataBroker getPingPongDataBroker() {
+        return lightyController.getServices().getPingPongDataBroker();
+    }
+
+    @Bean
+    public DOMDataTreeShardingService getDOMDataTreeShardingService() {
+        return lightyController.getServices().getDOMDataTreeShardingService();
+    }
+
+    @Bean
+    public DOMDataTreeService getDOMDataTreeService() {
+        return lightyController.getServices().getDOMDataTreeService();
+    }
+
+    @Bean
+    public DistributedShardFactory getDistributedShardFactory() {
+        return lightyController.getServices().getDistributedShardFactory();
+    }
+
+    @Bean
+    public DOMRpcService getDOMRpcService() {
+        return lightyController.getServices().getDOMRpcService();
+    }
+
+    @Bean
+    public DOMRpcProviderService getDOMRpcProviderService() {
+        return lightyController.getServices().getDOMRpcProviderService();
+    }
+
+    @Bean
+    public SchemaService getSchemaService() {
+        return lightyController.getServices().getSchemaService();
+    }
+
+    @Bean
+    public YangTextSourceProvider getYangTextSourceProvider() {
+        return lightyController.getServices().getYangTextSourceProvider();
+    }
+
+    @Bean
+    public BindingNormalizedNodeSerializer getBindingNormalizedNodeSerializer() {
+        return lightyController.getServices().getBindingNormalizedNodeSerializer();
+    }
+
+    @Bean
+    public BindingCodecTreeFactory getBindingCodecTreeFactory() {
+        return lightyController.getServices().getBindingCodecTreeFactory();
+    }
+
+    @Bean
+    public DOMEntityOwnershipService getDOMEntityOwnershipService() {
+        return lightyController.getServices().getDOMEntityOwnershipService();
+    }
+
+    @Bean
+    public EntityOwnershipService getEntityOwnershipService() {
+        return lightyController.getServices().getEntityOwnershipService();
+    }
+
+    @Bean
+    public LegacyEntityOwnershipServiceAdapter getLegacyEntityOwnershipService() {
+        return lightyController.getServices().getLegacyEntityOwnershipService();
+    }
+
+    @Bean
+    public ClusterAdminService getClusterAdminRPCService() {
+        return lightyController.getServices().getClusterAdminRPCService();
+    }
+
+    @Bean
+    public ClusterSingletonServiceProvider getClusterSingletonServiceProvider() {
+        return lightyController.getServices().getClusterSingletonServiceProvider();
+    }
+
+    @Bean
+    public RpcProviderRegistry getRpcProviderRegistry() {
+        return lightyController.getServices().getRpcProviderRegistry();
+    }
+
+    @Bean
+    public MountPointService getBindingMountPointService() {
+        return lightyController.getServices().getBindingMountPointService();
+    }
+
+    @Bean
+    public NotificationService getBindingNotificationService() {
+        return lightyController.getServices().getBindingNotificationService();
+    }
+
+    @Bean
+    public NotificationPublishService getBindingNotificationPublishService() {
+        return lightyController.getServices().getBindingNotificationPublishService();
+    }
+
+    @Bean
+    public NotificationProviderService getNotificationProviderService() {
+        return lightyController.getServices().getNotificationProviderService();
+    }
+
+    @Bean
+    public org.opendaylight.controller.sal.binding.api.NotificationService getNotificationService() {
+        return lightyController.getServices().getNotificationService();
+    }
+
+    @Bean(name = "BindingDataBroker")
+    public DataBroker getBindingDataBroker() {
+        return lightyController.getServices().getBindingDataBroker();
+    }
+
+    @Bean(name = "BindingPingPongDataBroker")
+    public DataBroker getBindingPingPongDataBroker() {
+        return lightyController.getServices().getBindingPingPongDataBroker();
+    }
+
+    @Bean
+    public EventExecutor getEventExecutor() {
+        return lightyController.getServices().getEventExecutor();
+    }
+
+    @Bean(name = "BossGroup")
+    public EventLoopGroup getBossGroup() {
+        return lightyController.getServices().getBossGroup();
+    }
+
+    @Bean(name = "WorkerGroup")
+    public EventLoopGroup getWorkerGroup() {
+        return lightyController.getServices().getWorkerGroup();
+    }
+
+    @Bean
+    public ThreadPool getThreadPool() {
+        return lightyController.getServices().getThreadPool();
+    }
+
+    @Bean
+    public ScheduledThreadPool getScheduledThreaPool() {
+        return lightyController.getServices().getScheduledThreaPool();
+    }
+
+    @Bean
+    public Timer getTimer() {
+        return lightyController.getServices().getTimer();
+    }
+
+}
