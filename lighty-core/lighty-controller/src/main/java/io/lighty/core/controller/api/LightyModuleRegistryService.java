d777a50496794280e92697460e5558df736576cd
Juraj Veverka <juraj.veverka@pantheon.tech>
2018-09-11 16:46:38 +0200

    License changed to Eclipse Public License v1.0

--- lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/api/LightyModuleRegistryService.java
+++ lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/api/LightyModuleRegistryService.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
+ */
 package io.lighty.core.controller.api;
 
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
