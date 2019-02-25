2a319dcf9f2c35e0ee78118b9965aa787e7363fa
Peter Suna <peter.suna@pantheon.tech>
2019-02-25 15:23:14 +0100

    Fix version typo and remove jetty.alpn.
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- lighty-examples/lighty-community-restconf-ofp-app/src/main/assembly/resources/start-ofp.sh
+++ lighty-examples/lighty-community-restconf-ofp-app/src/main/assembly/resources/start-ofp.sh
@@ -2,4 +2,4 @@
 
 # Starts application with default settings
 # alpn-boot version depends on java version 
-java -Xbootclasspath/p:lib/alpn-boot-8.1.13.v20181017.jar -jar lighty-community-restconf-ofp-app-9.1.0-SNAPSHOT.jar
+java -jar lighty-community-restconf-ofp-app-9.2.1-SNAPSHOT.jar
