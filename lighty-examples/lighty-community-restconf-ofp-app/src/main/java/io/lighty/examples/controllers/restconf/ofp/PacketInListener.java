05f03d4ec66dee0643e5f4424edbe6a347979ad3
Peter Suna <peter.suna@pantheon.tech>
2019-02-25 14:39:50 +0100

    Implement Lighty OpenFlow Plugin example
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ lighty-examples/lighty-community-restconf-ofp-app/src/main/java/io/lighty/examples/controllers/restconf/ofp/PacketInListener.java
@@ -0,0 +1,22 @@
+package io.lighty.examples.controllers.restconf.ofp;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketIn;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PacketInListener implements PacketProcessingListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PacketInListener.class);
+
+    public PacketInListener() {
+    }
+
+    @Override
+    public void onPacketReceived(PacketReceived notification) {
+        if (notification instanceof PacketIn){
+            LOG.trace("PacketIn Recived.",notification);
+        }
+    }
+}
