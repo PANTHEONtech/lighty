128b9776108d83df61fe77c4440007f6ced7dce7
Peter Piliar <peter.piliar@pantheon.tech>
2019-04-11 10:59:38 +0200

    Correct copyright information
    Signed-off-by: Peter Piliar <peter.piliar@pantheon.tech>

--- lighty-modules/lighty-ovsdb-sb/src/test/java/io/lighty/modules/southbound/ovsdb/OvsdbSouthboundPluginTest.java
+++ lighty-modules/lighty-ovsdb-sb/src/test/java/io/lighty/modules/southbound/ovsdb/OvsdbSouthboundPluginTest.java
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2018 Pantheon Technologies s.r.o. All Rights Reserved.
  *
- * This Source Code Form is subject to the terms of the LIGHTY.IO LICENSE,
- * version 1.1. If a copy of the license was not distributed with this file,
- * You can obtain one at https://lighty.io/license/1.1/
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
  */
 package io.lighty.modules.southbound.ovsdb;
 
