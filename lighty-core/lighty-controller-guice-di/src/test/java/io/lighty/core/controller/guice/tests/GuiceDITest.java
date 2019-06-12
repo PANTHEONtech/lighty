65da3dfd4ae710976001bb6153631b89a1e8c000
Juraj Veverka <juraj.veverka@pantheon.tech>
2019-06-12 17:03:49 +0200

    added license headers and fixed code review comments
    Signed-off-by: Juraj Veverka <juraj.veverka@pantheon.tech>

--- lighty-core/lighty-controller-guice-di/src/test/java/io/lighty/core/controller/guice/tests/GuiceDITest.java
+++ lighty-core/lighty-controller-guice-di/src/test/java/io/lighty/core/controller/guice/tests/GuiceDITest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
+ */
+
 package io.lighty.core.controller.guice.tests;
 
 import com.google.inject.Guice;
@@ -32,6 +40,17 @@ public class GuiceDITest {
         LightyControllerModule lightyModule = new LightyControllerModule(lightyController.getServices());
         Injector injector = Guice.createInjector(lightyModule);
         testService = injector.getInstance(TestService.class);
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        try {
+            if (lightyController != null) {
+                lightyController.shutdown();
+            }
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
     @Test
@@ -212,17 +231,6 @@ public class GuiceDITest {
     @Test
     public void testDIBindingDataBroker() {
         Assert.assertNotNull(testService.getBindingDataBroker());
-    }
-
-    @AfterClass
-    public static void shutdown() {
-        try {
-            if (lightyController != null) {
-                lightyController.shutdown();
-            }
-        } catch (Exception e) {
-            Assert.fail();
-        }
     }
 
 }
