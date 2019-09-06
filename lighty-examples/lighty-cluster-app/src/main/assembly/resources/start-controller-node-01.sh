b9257ec23ed96de735bf7beea790e077548ea4ed
Robert Varga <robert.varga@pantheon.tech>
2019-09-06 14:10:16 +0200

    Bump versions to 10.2.0-SNAPSHOT
    This starts the 10.2.x release train.
    
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- lighty-examples/lighty-cluster-app/src/main/assembly/resources/start-controller-node-01.sh
+++ lighty-examples/lighty-cluster-app/src/main/assembly/resources/start-controller-node-01.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #start controller with java 8
-java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-cluster-app-10.1.1-SNAPSHOT.jar -n 1 #-c configNode01.json
+java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-cluster-app-10.2.0-SNAPSHOT.jar -n 1 #-c configNode01.json
