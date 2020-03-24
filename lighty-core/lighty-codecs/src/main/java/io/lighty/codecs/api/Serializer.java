372df9827595465c23f81f625fadca7a28e2ba15
Michal Banik <banik.michal.sk@gmail.com>
2020-03-24 14:50:20 +0100

    simplify converting list in DataCodec
    revert back lighty codec to return Nullable
    
    Signed-off-by: Michal Banik <banik.michal.sk@gmail.com>
    
    (cherry picked from commit 85f27b4e65bcc63be990589992fc8e8096a87d64)

--- lighty-core/lighty-codecs/src/main/java/io/lighty/codecs/api/Serializer.java
+++ lighty-core/lighty-codecs/src/main/java/io/lighty/codecs/api/Serializer.java
@@ -8,7 +8,6 @@
 package io.lighty.codecs.api;
 
 import java.util.Collection;
-import java.util.Optional;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
@@ -32,7 +31,7 @@ public interface Serializer<BA extends DataObject> {
      * @param data Binding Independent data to be serialized
      * @return serialized Binding Aware data
      */
-    Optional<BA> convertToBindingAwareData(YangInstanceIdentifier identifier, NormalizedNode<?, ?> data);
+    BA convertToBindingAwareData(YangInstanceIdentifier identifier, NormalizedNode<?, ?> data);
 
     /**
      * Serialize the Binding Independent {@link MapNode} into Binding Aware data list.
