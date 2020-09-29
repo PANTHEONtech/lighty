952f3e0230d13020a2a6cb952d700dcfa96b5bea
Peter Valka <Peter.Valka@pantheon.tech>
2020-09-29 12:46:22 +0200

    Review Changes for fix readme versions
    Signed-off-by: Peter Valka <Peter.Valka@pantheon.tech>

--- lighty-examples/lighty-community-restconf-netconf-app/src/main/assembly/resources/start-controller.sh
+++ lighty-examples/lighty-community-restconf-netconf-app/src/main/assembly/resources/start-controller.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #start controller with java 11 or later
-#java --add-modules java.xml.bind -jar lighty-community-restconf-netconf-app-12.2.1-SNAPSHOT.jar
-# sampleConfigSingleNode.json
+#java --add-modules java.xml.bind -jar lighty-community-restconf-netconf-app-12.2.1-SNAPSHOT.jar sampleConfigSingleNode.json
