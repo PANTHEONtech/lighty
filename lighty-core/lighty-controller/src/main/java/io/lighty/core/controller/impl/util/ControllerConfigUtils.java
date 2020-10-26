409aa61f2231e1cfd02ec08830cb526112351843
Samuel Kontris <samuel.kontris@pantheon.tech>
2020-10-26 19:23:39 +0100

    [clustering] Add clustering
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>

--- lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/util/ControllerConfigUtils.java
+++ lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/util/ControllerConfigUtils.java
@@ -67,7 +67,9 @@ public final class ControllerConfigUtils {
         org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.clustering.prefix.shard
             .configuration.rev170110.$YangModuleInfoImpl.getInstance(),
         org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013
-            .$YangModuleInfoImpl.getInstance()
+            .$YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsal.core.general.entity.rev150930
+                .$YangModuleInfoImpl.getInstance()
     );
 
     public static final String CONTROLLER_CONFIG_ROOT_ELEMENT_NAME = "controller";
@@ -208,9 +210,9 @@ public final class ControllerConfigUtils {
         controllerConfiguration.getSchemaServiceConfig().setModels(allModels);
 
         controllerConfiguration.setConfigDatastoreContext(
-            DatastoreConfigurationUtils.createDefaultConfigDatastoreContext());
+                DatastoreConfigurationUtils.createDefaultConfigDatastoreContext());
         controllerConfiguration.setOperDatastoreContext(
-            DatastoreConfigurationUtils.createDefaultOperationalDatastoreContext());
+                DatastoreConfigurationUtils.createDefaultOperationalDatastoreContext());
 
         return controllerConfiguration;
     }
@@ -228,7 +230,7 @@ public final class ControllerConfigUtils {
         controllerConfiguration.getActorSystemConfig().setConfig(finalConfig);
     }
 
-    private static Config getAkkaConfigFromPath(final String pathToConfig) throws ConfigurationException {
+    public static Config getAkkaConfigFromPath(final String pathToConfig) throws ConfigurationException {
         Config akkaConfig = null;
         try {
             akkaConfig = ConfigFactory.parseFile(new File(pathToConfig));
