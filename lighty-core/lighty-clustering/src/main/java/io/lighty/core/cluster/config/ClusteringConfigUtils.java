f001e0a53a01642c68939ad9a06bbc66d90ee6c6
Samuel Kontris <samuel.kontris@pantheon.tech>
2020-11-18 18:24:42 +0100

    [clustering] Add improvements for clustering
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>

--- lighty-core/lighty-clustering/src/main/java/io/lighty/core/cluster/config/ClusteringConfigUtils.java
+++ lighty-core/lighty-clustering/src/main/java/io/lighty/core/cluster/config/ClusteringConfigUtils.java
@@ -10,6 +10,7 @@ package io.lighty.core.cluster.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,12 @@ public final class ClusteringConfigUtils {
     private static final Logger LOG = LoggerFactory.getLogger(ClusteringConfigUtils.class);
 
     public static final String MODULE_SHARDS_TMP_PATH = "/tmp/module-shards.conf";
+    public static final String AKKA_DISCOVERY_METHOD_PATH = "akka.discovery.method";
+
+    public static final String K8S_DISCOVERY_API_NAME = "kubernetes-api";
     public static final String K8S_POD_RESTART_TIMEOUT_PATH = "akka.lighty-kubernetes.pod-restart-timeout";
+    public static final String K8S_POD_NAMESPACE_PATH = "akka.discovery.kubernetes-api.pod-namespace";
+    public static final String K8S_POD_LABEL_SELECTOR_PATH = "akka.discovery.kubernetes-api.pod-label-selector";
 
     private ClusteringConfigUtils() {
         // this class should not be instantiated
@@ -26,7 +32,9 @@ public final class ClusteringConfigUtils {
 
     /**
      * Generate content of a Module-Shards.conf that specifies the members on which the Shards should be replicated.
+     *
      * @param memberRoles - roles (members) to which the module shards should be replicated to
+     * @return generated content
      */
     public static String generateModuleShardsForMembers(List<String> memberRoles) {
         return String.format("module-shards = [%n%s]", String.join(",\n",
@@ -37,8 +45,30 @@ public final class ClusteringConfigUtils {
     }
 
     public static boolean isKubernetesDeployment(Config actorSystemConfig) {
-        return actorSystemConfig.hasPath("akka.discovery.method")
-                && actorSystemConfig.getString("akka.discovery.method").equalsIgnoreCase("kubernetes-api");
+        return actorSystemConfig.hasPath(AKKA_DISCOVERY_METHOD_PATH)
+                && actorSystemConfig.getString(AKKA_DISCOVERY_METHOD_PATH).equalsIgnoreCase(K8S_DISCOVERY_API_NAME);
+    }
+
+    /**
+     * Reads pod-namespace from akka.discovery.kubernetes-api.
+     *
+     * @param actorSystemConfig provided akka configuration
+     * @return configured pod-namespace value
+     */
+    public static Optional<String> getPodNamespaceFromConfig(Config actorSystemConfig) {
+        return actorSystemConfig.hasPath(K8S_POD_NAMESPACE_PATH)
+                ? Optional.of(actorSystemConfig.getString(K8S_POD_NAMESPACE_PATH)) : Optional.empty();
+    }
+
+    /**
+     * Reads pod-label-selector from akka.discovery.kubernetes-api.
+     *
+     * @param actorSystemConfig provided akka configuration
+     * @return configured pod-label-selector value
+     */
+    public static Optional<String> getPodSelectorFromConfig(Config actorSystemConfig) {
+        return actorSystemConfig.hasPath(K8S_POD_LABEL_SELECTOR_PATH)
+                ? Optional.of(actorSystemConfig.getString(K8S_POD_LABEL_SELECTOR_PATH)) : Optional.empty();
     }
 
     private static String generateShard(String name, List<String> replicas) {
