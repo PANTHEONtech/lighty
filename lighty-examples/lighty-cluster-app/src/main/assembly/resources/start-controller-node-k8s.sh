3ddcf35a613c5741d4de4781c169b86ce35f840e
Michal Banik <michal.banik@pantheon.tech>
2020-11-30 13:53:57 +0100

    Bump versions for Aluminum SR1 developer branch for clustering example
    Signed-off-by: Michal Banik <michal.banik@pantheon.tech>

--- lighty-examples/lighty-cluster-app/src/main/assembly/resources/start-controller-node-k8s.sh
+++ lighty-examples/lighty-cluster-app/src/main/assembly/resources/start-controller-node-k8s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #start controller
-cd /lighty-cluster-app-13.0.2-SNAPSHOT
-java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-cluster-app-13.0.2-SNAPSHOT.jar -n 0 -k
+cd /lighty-cluster-app-13.1.0-SNAPSHOT
+java -ms128m -mx128m -XX:MaxMetaspaceSize=128m -jar lighty-cluster-app-13.1.0-SNAPSHOT.jar -n 0 -k
