8a477ec3909289128988644a71156c030a45d238
marekzatko <Marek.Zatko@pantheon.tech>
2020-12-07 11:11:29 +0100

    Use bigger sleep value after test
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>

--- lighty-modules/lighty-openflow-sb/src/test/java/io/lighty/modules/southbound/openflow/tests/OpenflowSouthboundPluginTestBase.java
+++ lighty-modules/lighty-openflow-sb/src/test/java/io/lighty/modules/southbound/openflow/tests/OpenflowSouthboundPluginTestBase.java
@@ -37,7 +37,7 @@ public abstract class OpenflowSouthboundPluginTestBase {
 
     private static final Logger LOG = LoggerFactory.getLogger(OpenflowSouthboundPluginTestBase.class);
     public static final long SHUTDOWN_TIMEOUT_MILLIS = 15_000;
-    public static final long SLEEP_AFTER_SHUTDOWN_TIMEOUT_MILLIS = 5_000;
+    public static final long SLEEP_AFTER_SHUTDOWN_TIMEOUT_MILLIS = 10_000;
 
     private LightyController lightyController;
     private CommunityRestConf communityRestConf;
