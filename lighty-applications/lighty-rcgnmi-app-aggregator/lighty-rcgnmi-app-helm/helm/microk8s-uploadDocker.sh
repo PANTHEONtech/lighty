c7349639fb446228b8bf0d1c476096fab48b7fad
marekzatko <Marek.Zatko@pantheon.tech>
2021-07-06 08:47:29 +0200

    Uncomment rm docker.tar
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>

--- lighty-applications/lighty-rcgnmi-app-aggregator/lighty-rcgnmi-app-helm/helm/microk8s-uploadDocker.sh
+++ lighty-applications/lighty-rcgnmi-app-aggregator/lighty-rcgnmi-app-helm/helm/microk8s-uploadDocker.sh
@@ -5,8 +5,8 @@ docker save --output="./lighty-rcgnmi:latest.tar" lighty-rcgnmi:latest
 
 echo "[2/3] Uploading exported .tar to microk8s ..."
 microk8s ctr --namespace k8s.io image import lighty-rcgnmi:latest.tar
-#
-#echo "[3/3] Removing exported .tar ..."
-#rm lighty-rcgnmi:latest.tar
-#
+
+echo "[3/3] Removing exported .tar ..."
+rm lighty-rcgnmi:latest.tar
+
 #echo "[done]"
