b19ba0ccd19101fc3685b54969032c0219582a35
Filip Čúzy <filip.cuzy@pantheon.local>
2021-07-09 12:49:29 +0200

    Edit copyright
    Signed-off-by: Filip Čúzy <filip.cuzy@pantheon.local>

--- lighty-applications/lighty-rnc-app-aggregator/lighty-rnc-module/src/main/java/io/lighty/applications/rnc/module/RncLightyModule.java
+++ lighty-applications/lighty-rnc-app-aggregator/lighty-rnc-module/src/main/java/io/lighty/applications/rnc/module/RncLightyModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Pantheon Technologies s.r.o. All Rights Reserved.
+ * Copyright (c) 2021 PANTHEON.tech s.r.o. All Rights Reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
