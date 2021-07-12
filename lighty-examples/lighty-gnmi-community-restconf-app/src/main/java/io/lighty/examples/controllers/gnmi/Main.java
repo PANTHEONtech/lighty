43f6de3c7621f9054d3b56bb3d65a7160cdb9947
PeterSuna <Peter.Suna@pantheon.tech>
2021-07-12 01:59:44 -0700

    Update example Main class to support tests
    Signed-off-by: PeterSuna <Peter.Suna@pantheon.tech>

--- lighty-examples/lighty-gnmi-community-restconf-app/src/main/java/io/lighty/examples/controllers/gnmi/Main.java
+++ lighty-examples/lighty-gnmi-community-restconf-app/src/main/java/io/lighty/examples/controllers/gnmi/Main.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 
 public final class Main {
     private static final String GNMI_CONFIGURATION = "/example_config.json";
+    private static final String TEST_CONFIG = "/src/test/resources/example_config.json";
+    private static final String PATH_TO_ASSEMBLY_RESOURCE = "/src/main/assembly/resources";
+    private static final String MODULE_NAME = "lighty-gnmi-community-restconf-app";
     private static final String ASSEMBLY_RESOURCES
             = "/lighty-examples/lighty-gnmi-community-restconf-app/src/main/assembly/resources";
     private static final String LIGHTY = "lighty";
@@ -23,10 +26,18 @@ public final class Main {
         // Find current location of resources
         final String gnmiConfig;
         if (currentFolder.length() > 6 && currentFolder.endsWith(LIGHTY)) {
+            // Example stared by IDEA
             currentFolder += ASSEMBLY_RESOURCES;
-            // Use different gNMI configuration when is example start in IDEA
-            gnmiConfig = ASSEMBLY_RESOURCES + GNMI_CONFIGURATION;
-        } else {
+            // Specific gNMI configuration for IDEA run
+            gnmiConfig = currentFolder + GNMI_CONFIGURATION;
+        } else if (currentFolder.endsWith(MODULE_NAME)) {
+            // Example run by tests
+            // Specific gNMI config for tests
+            gnmiConfig = currentFolder + TEST_CONFIG;
+            currentFolder += PATH_TO_ASSEMBLY_RESOURCE;
+        }
+        else {
+            // Example stared from jar file
             gnmiConfig = currentFolder + GNMI_CONFIGURATION;
         }
 
