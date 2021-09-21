01208d0ed5da2e6e5758a402202c102ecd6f7177
Peter Suna <peter.suna@pantheon.tech>
2021-09-21 13:04:25 +0200

    Revert vulnerability marked as "Won't fix"
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/services/UserAccessServiceImpl.java
+++ lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/services/UserAccessServiceImpl.java
@@ -30,11 +30,11 @@ public class UserAccessServiceImpl implements UserAccessService {
     public Optional<UserData> login(String sessionId, LoginRequest loginRequest) {
         UserData userData = users.get(loginRequest.getUserName());
         if (userData != null && userData.verifyPassword(loginRequest.getPassword())) {
-            LOG.info("login OK: {}", sessionId);
+            LOG.info("login OK: {} {}", sessionId, loginRequest.getUserName());
             sessions.put(sessionId, userData);
             return Optional.of(userData);
         }
-        LOG.info("login Failed: {}", sessionId);
+        LOG.info("login Failed: {} {}", sessionId, loginRequest.getUserName());
         return Optional.empty();
     }
 
