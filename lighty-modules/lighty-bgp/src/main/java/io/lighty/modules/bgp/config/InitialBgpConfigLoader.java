b10684a573a0ea75cba86d46a3f199891975ce43
marekzatko <Marek.Zatko@pantheon.tech>
2021-11-26 16:14:59 +0100

    Update/add copyright
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>

--- lighty-modules/lighty-bgp/src/main/java/io/lighty/modules/bgp/config/InitialBgpConfigLoader.java
+++ lighty-modules/lighty-bgp/src/main/java/io/lighty/modules/bgp/config/InitialBgpConfigLoader.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2021 PANTHEON.tech s.r.o. All Rights Reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
  */
 package io.lighty.modules.bgp.config;
 
