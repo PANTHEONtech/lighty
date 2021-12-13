2a90363bbaaae2df1d794f434265009971788d9c
marekzatko <Marek.Zatko@pantheon.tech>
2021-12-13 09:17:31 +0100

    Remove unusued constructors from codecs.util.exceptions
    Signed-off-by: marekzatko <Marek.Zatko@pantheon.tech>
    (cherry picked from commit 4105606d2661c031977fb85ea6f1eb98d47127d9)

--- lighty-core/lighty-codecs-util/src/main/java/io/lighty/codecs/util/exception/DeserializationException.java
+++ lighty-core/lighty-codecs-util/src/main/java/io/lighty/codecs/util/exception/DeserializationException.java
@@ -16,8 +16,4 @@ public class DeserializationException extends Exception {
     public DeserializationException(final Throwable cause) {
         super(cause);
     }
-
-    public DeserializationException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
 }
