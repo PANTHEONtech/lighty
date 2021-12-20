e57f6921379ab4670c09be222274e1e0f0f05c64
tobias.pobocik <tobias.pobocik@pantheon.tech>
2021-12-20 16:33:34 +0100

    Added tests for swagger in lighty-rnc-app
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-applications/lighty-rnc-app-aggregator/lighty-rnc-app/src/test/java/io/lighty/applications/rnc/app/MainTest.java
+++ lighty-applications/lighty-rnc-app-aggregator/lighty-rnc-app/src/test/java/io/lighty/applications/rnc/app/MainTest.java
@@ -16,9 +16,20 @@ import static org.mockito.Mockito.verify;
 
 import com.google.common.util.concurrent.Futures;
 import io.lighty.applications.rnc.module.RncLightyModule;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest {
+
+    private static final Duration REQUEST_TIMEOUT_DURATION = Duration.ofMillis(10_000L);
 
     @Test
     public void testStartWithDefaultConfiguration() {
@@ -45,5 +56,31 @@ public class MainTest {
         Main app = spy(new Main());
         verify(app, never()).createRncLightyModule(any());
         app.start(new String[] {"-c","no_config.json"});
+    }
+
+    @Test
+    public void swaggerURLsTest() throws IOException, InterruptedException {
+        Main app = new Main();
+        app.start(new String[] {});
+        HttpResponse<String> operations;
+        operations = GET("http://localhost:8888/","apidoc/openapi3/18/apis/single");
+        Assert.assertEquals(operations.statusCode(), 200);
+        operations = GET("http://localhost:8888/","apidoc/explorer/index.html");
+        Assert.assertEquals(operations.statusCode(), 200);
+    }
+
+    //used for testing swagger for HTTP GET response
+    public HttpResponse<String> GET(String baseUrl,String uri) throws InterruptedException, IOException {
+
+        ExecutorService httpClientExecutor = Executors.newSingleThreadExecutor();
+        HttpClient httpClient = HttpClient.newBuilder().executor(httpClientExecutor).build();
+
+        final HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + uri))
+                .header("Content-Type", "application/json")
+                .GET()
+                .timeout(REQUEST_TIMEOUT_DURATION)
+                .build();
+        return httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
     }
 }
