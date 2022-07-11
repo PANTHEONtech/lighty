0c9f58c62952440a757af43047861369ad9c6b0a
Peter Suna <peter.suna@pantheon.tech>
2022-07-11 15:07:37 +0200

    Cleanup gNMI SecurityFactory
    Remove unused constructors
    
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-connector/src/main/java/io/lighty/modules/gnmi/connector/configuration/SecurityFactory.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-connector/src/main/java/io/lighty/modules/gnmi/connector/configuration/SecurityFactory.java
@@ -11,20 +11,12 @@ package io.lighty.modules.gnmi.connector.configuration;
 import io.lighty.modules.gnmi.connector.security.Security;
 import io.lighty.modules.gnmi.connector.session.api.SessionManager;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Collection;
 
 /**
@@ -34,24 +26,7 @@ import java.util.Collection;
 public final class SecurityFactory {
 
     private SecurityFactory() {
-        throw new UnsupportedOperationException("Instance of this class should not be created!");
-    }
-
-    public static Security createGnmiSecurity(final SouthboundConfiguration configuration)
-            throws CertificateException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        return createGnmiSecurity(
-                Paths.get(configuration.getCaCertificatePaths()),
-                Paths.get(configuration.getClientCertificatesChainPath()),
-                Paths.get(configuration.getPrivateKeyPath()));
-    }
-
-    public static Security createGnmiSecurity(final Path caCertificatePath, final Path clientCertificatePath,
-                                              final Path privateKeyPath)
-            throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, CertificateException {
-        return new Security(
-                loadCertsFromFile(caCertificatePath),
-                loadCertsFromFile(clientCertificatePath),
-                loadPrivateKeyFromFile(privateKeyPath));
+        // Hidden on purpose
     }
 
     public static Security createGnmiSecurity(final String caCertificate, final String clientCertificate,
@@ -66,14 +41,6 @@ public final class SecurityFactory {
         return new Security();
     }
 
-
-    private static Collection<X509Certificate> loadCertsFromFile(final Path certsFilePath) throws IOException,
-            CertificateException {
-        try (InputStream certsInputStream = Files.newInputStream(certsFilePath)) {
-            return getX509Certificates(certsInputStream);
-        }
-    }
-
     private static Collection<X509Certificate> loadCertificates(final String certificate) throws CertificateException {
         return getX509Certificates(new ByteArrayInputStream(certificate.getBytes(Charset.defaultCharset())));
     }
@@ -83,12 +50,5 @@ public final class SecurityFactory {
             throws CertificateException {
         return (Collection<X509Certificate>) CertificateFactory.getInstance("X.509")
                 .generateCertificates(certsInputStream);
-    }
-
-    private static PrivateKey loadPrivateKeyFromFile(final Path keyFile) throws IOException, NoSuchAlgorithmException,
-            InvalidKeySpecException {
-        final byte[] keyBytes = Files.readAllBytes(keyFile);
-        return KeyFactory.getInstance("RSA")
-                .generatePrivate(new PKCS8EncodedKeySpec(keyBytes));
     }
 }
