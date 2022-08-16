5a8a77f347b63415d40d88e0dc230c5758fac4f8
tobias.pobocik <tobias.pobocik@pantheon.tech>
2022-08-16 10:02:35 +0200

    Add semVer workaround
    - SemVer is needed here so that the GNMI sends back capabilities properly
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-device-simulator/src/main/java/io/lighty/modules/gnmi/simulatordevice/gnmi/GnmiCapabilitiesService.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-device-simulator/src/main/java/io/lighty/modules/gnmi/simulatordevice/gnmi/GnmiCapabilitiesService.java
@@ -16,9 +16,13 @@ import java.util.Objects;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.SemVer;
-import org.opendaylight.yangtools.openconfig.model.api.OpenConfigVersionEffectiveStatement;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.ri.stmt.impl.decl.UnrecognizedStatementImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +30,8 @@ public class GnmiCapabilitiesService {
 
     private static final Logger LOG = LoggerFactory.getLogger(GnmiCapabilitiesService.class);
     private static final String GNMI_VERSION = "0.7.0";
+    private static final XMLNamespace OC_NS = XMLNamespace.of("http://openconfig.net/yang/openconfig-ext").intern();
+    private static final QName OC_VERSION = QName.create(OC_NS, Revision.of("2020-06-16"), "openconfig-version");
 
     private final EffectiveModelContext schemaContext;
     private final EnumSet<Gnmi.Encoding> supportedEncodings;
@@ -46,8 +52,7 @@ public class GnmiCapabilitiesService {
             final Gnmi.ModelData.Builder builder = Gnmi.ModelData.newBuilder()
                 .setOrganization(module.getOrganization().orElse("UNKNOWN-ORGANIZATION"))
                 .setName(module.getName());
-            final Optional<SemVer> optSemVer = module.asEffectiveStatement()
-                    .findFirstEffectiveSubstatementArgument(OpenConfigVersionEffectiveStatement.class);
+            final Optional<SemVer> optSemVer = getSemVer(module);
             if (optSemVer.isPresent()) {
                 builder.setVersion(optSemVer.get().toString());
             } else if (module.getRevision().isPresent()) {
@@ -62,4 +67,14 @@ public class GnmiCapabilitiesService {
             .setGNMIVersion(GNMI_VERSION)
             .build();
     }
+
+    private Optional<SemVer> getSemVer(final Module module) {
+            final var decStmt = module.asEffectiveStatement().getDeclared().declaredSubstatements();
+            return ((List<? extends DeclaredStatement<?>>) decStmt).stream()
+                    .filter(UnrecognizedStatementImpl.class::isInstance)
+                    .map(UnrecognizedStatementImpl.class::cast)
+                    .filter(t -> t.statementDefinition().getStatementName().equals(OC_VERSION))
+                    .findAny()
+                    .map(stmt -> SemVer.valueOf(stmt.argument().toString()));
+        }
 }
