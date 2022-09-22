a2610385b8f05e3779fe6024e0b0192d51fc321d
tobias.pobocik <tobias.pobocik@pantheon.tech>
2022-09-22 08:56:59 +0200

    Add test assuring yang-patch operation
    Add test which assures yang-patch Replace operation with
    previously added instance-identifier-patch-module model.
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    (cherry picked from commit 78e35678744e6a2f3c1bbaa44863ba4364685e25)

---
+++ lighty-modules/lighty-restconf-nb-community/src/test/java/io/lighty/modules/northbound/restconf/community/impl/tests/YangPatchTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech s.r.o. All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
+ */
+package io.lighty.modules.northbound.restconf.community.impl.tests;
+
+import static org.opendaylight.restconf.common.patch.PatchEditOperation.REPLACE;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.restconf.common.context.InstanceIdentifierContext;
+import org.opendaylight.restconf.common.patch.PatchContext;
+import org.opendaylight.restconf.common.patch.PatchEntity;
+import org.opendaylight.restconf.common.patch.PatchStatusContext;
+import org.opendaylight.restconf.common.patch.PatchStatusEntity;
+import org.opendaylight.restconf.nb.rfc8040.rests.transactions.MdsalRestconfStrategy;
+import org.opendaylight.restconf.nb.rfc8040.rests.utils.PatchDataTransactionUtil;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.SystemMapNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapNodeBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class YangPatchTest extends CommunityRestConfTestBase {
+
+    private static final QName BASE_Q_NAME = QName.create("instance:identifier:patch:module",
+            "instance-identifier-patch-module", Revision.of("2015-11-21"));
+    private static final QName EXAMPLE_LIST = QName.create(BASE_Q_NAME, "my-list1");
+    private static final QName EXAMPLE_LIST_NAME =  QName.create(BASE_Q_NAME, "name");
+    private static final NodeIdentifierWithPredicates NODE_WITH_KEY = NodeIdentifierWithPredicates.of((EXAMPLE_LIST));
+    private static final QName MY_LEAF_11 =  QName.create(EXAMPLE_LIST, "my-leaf11");
+    private static final QName MY_LEAF_12 = QName.create(EXAMPLE_LIST, "my-leaf12");
+    private static final QName CONTAINER_ID = QName.create(EXAMPLE_LIST, "patch-cont");
+    private static final String MY_LIST_1_A = "my-list1 - A";
+    private static final String I_AM_LEAF_11_0 = "I am leaf11-0";
+    private static final String I_AM_LEAF_12_1 = "I am leaf12-1";
+
+    @Test
+    public void patchDataReplaceTest() throws Exception {
+        Assert.assertNotNull(getLightyController());
+        Assert.assertNotNull(getCommunityRestConf());
+
+        final ContainerNode patchContainerNode = getContainerWithData();
+        final YangInstanceIdentifier targetNodeMerge = YangInstanceIdentifier.builder()
+                .node(CONTAINER_ID)
+                .build();
+
+        final PatchEntity entityReplace = new PatchEntity("edit1", REPLACE, targetNodeMerge, patchContainerNode);
+        final InstanceIdentifierContext iidContext = InstanceIdentifierContext.ofLocalRoot(
+                        getLightyController().getServices().getDOMSchemaService().getGlobalContext());
+        final PatchContext patchContext = new PatchContext(iidContext, List.of(entityReplace), "test-patch");
+
+        final PatchStatusContext patchStatusContext = PatchDataTransactionUtil.patchData(patchContext,
+                new MdsalRestconfStrategy(getLightyController().getServices().getClusteredDOMDataBroker()),
+                getLightyController().getServices().getDOMSchemaService().getGlobalContext());
+        for (final PatchStatusEntity entity : patchStatusContext.getEditCollection()) {
+            assertTrue(entity.isOk());
+        }
+        assertTrue(patchStatusContext.isOk());
+
+        final ContainerNode response = (ContainerNode) getLightyController().getServices().getClusteredDOMDataBroker()
+                .newReadOnlyTransaction()
+                .read(LogicalDatastoreType.CONFIGURATION, iidContext.getInstanceIdentifier())
+                .get(5000, TimeUnit.MILLISECONDS).get();
+
+        final DataContainerChild bodyOfResponse = response.body().iterator().next();
+        Assert.assertEquals(bodyOfResponse, getContainerWithData());
+    }
+
+    private static ContainerNode getContainerWithData() {
+        final LeafNode<?> nameLeafA = Builders.leafBuilder()
+                .withNodeIdentifier(new NodeIdentifier(EXAMPLE_LIST_NAME))
+                .withValue(MY_LIST_1_A)
+                .build();
+        final LeafNode<?> buildLeaf1 = Builders.leafBuilder()
+                .withNodeIdentifier(new NodeIdentifier(MY_LEAF_11))
+                .withValue(I_AM_LEAF_11_0)
+                .build();
+        final LeafNode<?> buildLeaf2 = Builders.leafBuilder()
+                .withNodeIdentifier(new NodeIdentifier(MY_LEAF_12))
+                .withValue(I_AM_LEAF_12_1)
+                .build();
+        final MapEntryNode mapEntryNode = Builders.mapEntryBuilder()
+                .withNodeIdentifier(NODE_WITH_KEY)
+                .withValue(List.of(nameLeafA, buildLeaf1, buildLeaf2))
+                .build();
+        final SystemMapNode myList = ImmutableMapNodeBuilder.create()
+                .withNodeIdentifier(new NodeIdentifier(EXAMPLE_LIST))
+                .withValue(Collections.singletonList(mapEntryNode))
+                .build();
+        return Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(CONTAINER_ID))
+                .withValue(Collections.singletonList(myList))
+                .build();
+    }
+}
