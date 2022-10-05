d1c5b3b1082af2ece72df97829125b47d51bdaac
tobias.pobocik <tobias.pobocik@pantheon.tech>
2022-10-05 14:30:05 +0200

    Improve ModulesConfig Test
    This test was missing assertion for .equals and .hashcode method
    which was also reported inside sonarcloud.
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-applications/lighty-app-modules-config/src/test/java/io/lighty/applications/util/ModulesConfigTest.java
+++ lighty-applications/lighty-app-modules-config/src/test/java/io/lighty/applications/util/ModulesConfigTest.java
@@ -8,6 +8,9 @@
 package io.lighty.applications.util;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertTrue;
 
 import io.lighty.core.controller.impl.config.ConfigurationException;
 import org.junit.jupiter.api.Test;
@@ -28,5 +31,29 @@ public class ModulesConfigTest {
                 .getResourceAsStream("missingModulesConfig.json"));
 
         assertEquals(config.getModuleTimeoutSeconds(), 60);
+    }
+
+    @Test
+    void configEqualsTest() {
+        final ModulesConfig firstConfig = new ModulesConfig();
+        firstConfig.setModuleTimeoutSeconds(30);
+        final ModulesConfig secondConfig = new ModulesConfig();
+        secondConfig.setModuleTimeoutSeconds(60);
+
+        assertFalse(firstConfig.equals(secondConfig));
+        secondConfig.setModuleTimeoutSeconds(30);
+        assertTrue(firstConfig.equals(secondConfig));
+    }
+
+    @Test
+    void configHashCodeTest() {
+        final ModulesConfig firstConfig = new ModulesConfig();
+        firstConfig.setModuleTimeoutSeconds(30);
+        final ModulesConfig secondConfig = new ModulesConfig();
+        secondConfig.setModuleTimeoutSeconds(60);
+
+        assertNotEquals(firstConfig.hashCode(), secondConfig.hashCode());
+        secondConfig.setModuleTimeoutSeconds(30);
+        assertEquals(firstConfig.hashCode(), secondConfig.hashCode());
     }
 }
