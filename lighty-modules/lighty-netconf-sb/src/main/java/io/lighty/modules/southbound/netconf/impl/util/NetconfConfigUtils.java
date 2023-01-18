2ce52fce3d8c67b4d48bd48769f44aa0adbe36de
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-01-18 09:21:22 +0100

    Use newer revision of netconf-node-{optional,topology}
    Revision changed as a result of the following commit:
    https://github.com/opendaylight/netconf/commit/22d8a6b30c7678b605a384f46a930a8b543de34a
    
    JIRA: LIGHTY-108
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-netconf-sb/src/main/java/io/lighty/modules/southbound/netconf/impl/util/NetconfConfigUtils.java
+++ lighty-modules/lighty-netconf-sb/src/main/java/io/lighty/modules/southbound/netconf/impl/util/NetconfConfigUtils.java
@@ -47,9 +47,9 @@ public final class NetconfConfigUtils {
     public static final Set<YangModuleInfo> NETCONF_TOPOLOGY_MODELS = Set.of(
             org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017
                     .$YangModuleInfoImpl.getInstance(),
-            org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114
+            org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev221225
                     .$YangModuleInfoImpl.getInstance(),
-            org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.optional.rev190614
+            org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.optional.rev221225
                     .$YangModuleInfoImpl.getInstance(),
             org.opendaylight.yang.gen.v1.urn.opendaylight.yang.extension.yang.ext.rev130709
                     .$YangModuleInfoImpl.getInstance(),
