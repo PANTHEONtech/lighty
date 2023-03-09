PANTHEONtech/lighty

commit d35ab93f16e4e9d6ea4eb9c51275934d709cb3cc

Eliminate references to SchemaContextHandler
Eliminate references to SchemaContextHandler, and replace
it with DatabindContext, hence it is an immutable view of these.

https://github.com/opendaylight/netconf/commit/32013f8d8fefcb6f5e70f6b809e7ee5291d732a6

JIRA: LIGHTY-108
Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

lighty-modules/lighty-restconf-nb-community/src/main/java/io/lighty/modules/northbound/restconf/community/impl/CommunityRestConf.java
@@ -28,6 +28,8 @@ import org.opendaylight.mdsal.dom.api.DOMNotificationService;
 import org.opendaylight.mdsal.dom.api.DOMRpcService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.restconf.nb.rfc8040.RestconfApplication;
+import org.opendaylight.restconf.nb.rfc8040.databind.DatabindProvider;
+import org.opendaylight.restconf.nb.rfc8040.databind.mdsal.DOMDatabindProvider;
 import org.opendaylight.restconf.nb.rfc8040.handlers.SchemaContextHandler;
 import org.opendaylight.restconf.nb.rfc8040.streams.Configuration;
 import org.slf4j.Logger;
@@ -48,7 +50,6 @@ public class CommunityRestConf extends AbstractLightyModule {
     private final String restconfServletContextPath;
     private Server jettyServer;
     private LightyServerBuilder lightyServerBuilder;
-    private SchemaContextHandler schemaCtxHandler;
 
     public CommunityRestConf(final DOMDataBroker domDataBroker, final DOMRpcService domRpcService,
             final DOMActionService domActionService, final DOMNotificationService domNotificationService,
@@ -81,15 +82,12 @@ public class CommunityRestConf extends AbstractLightyModule {
     @Override
     protected boolean initProcedure() {
         final Stopwatch stopwatch = Stopwatch.createStarted();
-
-        this.schemaCtxHandler
-                = new SchemaContextHandler(this.domDataBroker, this.domSchemaService);
-        this.schemaCtxHandler.init();
         final Configuration streamsConfiguration = RestConfConfigUtils.getStreamsConfiguration();
+        new SchemaContextHandler(this.domDataBroker, this.domSchemaService);
+        final DatabindProvider databindProvider = new DOMDatabindProvider(domSchemaService);
 
         LOG.info("Starting RestconfApplication with configuration {}", streamsConfiguration);
-
-        final RestconfApplication restconfApplication = new RestconfApplication(schemaCtxHandler,
+        final RestconfApplication restconfApplication = new RestconfApplication(databindProvider,
                 this.domMountPointService, this.domDataBroker, this.domRpcService, this.domActionService,
                 this.domNotificationService, this.domSchemaService, streamsConfiguration);
         final ServletContainer servletContainer8040 = new ServletContainer(ResourceConfig
@@ -133,10 +131,6 @@ public class CommunityRestConf extends AbstractLightyModule {
     @Override
     protected boolean stopProcedure() {
         boolean stopFailed = false;
-        if (this.schemaCtxHandler != null) {
-            this.schemaCtxHandler.close();
-            LOG.info("Schema context handler closed");
-        }
         if (this.jettyServer != null) {
             try {
                 this.jettyServer.stop();

commit 571ff8459f96ab5ce8a5394eb911c234c489e621

Fix SchemaContextHandler closing
Close SchemaContextHandler initialized in initProcedure
in stopProcedure method when we no more need it.

Problem is caused by: d35ab93f16e4e9d6ea4eb9c51275934d709cb3cc.

Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

lighty-modules/lighty-restconf-nb-community/src/main/java/io/lighty/modules/northbound/restconf/community/impl/CommunityRestConf.java
@@ -50,6 +50,7 @@ public class CommunityRestConf extends AbstractLightyModule {
     private final String restconfServletContextPath;
     private Server jettyServer;
     private LightyServerBuilder lightyServerBuilder;
+    private SchemaContextHandler schemaCtxHandler;
 
     public CommunityRestConf(final DOMDataBroker domDataBroker, final DOMRpcService domRpcService,
             final DOMActionService domActionService, final DOMNotificationService domNotificationService,
@@ -83,10 +84,11 @@ public class CommunityRestConf extends AbstractLightyModule {
     protected boolean initProcedure() {
         final Stopwatch stopwatch = Stopwatch.createStarted();
         final Configuration streamsConfiguration = RestConfConfigUtils.getStreamsConfiguration();
-        new SchemaContextHandler(this.domDataBroker, this.domSchemaService);
-        final DatabindProvider databindProvider = new DOMDatabindProvider(domSchemaService);
-
         LOG.info("Starting RestconfApplication with configuration {}", streamsConfiguration);
+
+        this.schemaCtxHandler = new SchemaContextHandler(this.domDataBroker, this.domSchemaService);
+
+        final DatabindProvider databindProvider = new DOMDatabindProvider(domSchemaService);
         final RestconfApplication restconfApplication = new RestconfApplication(databindProvider,
                 this.domMountPointService, this.domDataBroker, this.domRpcService, this.domActionService,
                 this.domNotificationService, this.domSchemaService, streamsConfiguration);
@@ -131,6 +133,10 @@ public class CommunityRestConf extends AbstractLightyModule {
     @Override
     protected boolean stopProcedure() {
         boolean stopFailed = false;
+        if (this.schemaCtxHandler != null) {
+            this.schemaCtxHandler.close();
+            LOG.info("Schema context handler closed");
+        }
         if (this.jettyServer != null) {
             try {
                 this.jettyServer.stop();