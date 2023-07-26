b460a8f369e9c023915364d0b51702ebc4e3bd8b
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-07-26 20:41:55 +0200

    Add charset to YangTextSchemaSource
    YangTextSchemaSource now requires Charset to be specified.
    https://github.com/opendaylight/yangtools/commit/d95368f629eb95d9e620d6394d445b689ad7bd11#diff-4d088af7ba991550bb26af0938006958ca91b708837f1ea6970e11815665b428R60
    
    JIRA: LIGHTY-235
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-device-simulator/src/main/java/io/lighty/modules/gnmi/simulatordevice/utils/EffectiveModelContextBuilder.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-device-simulator/src/main/java/io/lighty/modules/gnmi/simulatordevice/utils/EffectiveModelContextBuilder.java
@@ -12,6 +12,7 @@ import com.google.common.io.ByteSource;
 import io.lighty.modules.gnmi.commons.util.YangModelSanitizer;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -112,7 +113,7 @@ public class EffectiveModelContextBuilder {
                 final YangStatementStreamSource statementSource = YangStatementStreamSource.create(
                         YangTextSchemaSource.delegateForByteSource(
                                 YangTextSchemaSource.identifierFromFilename(file.getName()),
-                                sanitizedYangByteSource));
+                                sanitizedYangByteSource, Charset.defaultCharset()));
 
                 sourceArrayList.add(statementSource);
             }
@@ -134,7 +135,7 @@ public class EffectiveModelContextBuilder {
                 final YangStatementStreamSource statementSource
                         = YangStatementStreamSource.create(YangTextSchemaSource.delegateForByteSource(
                         YangTextSchemaSource.identifierFromFilename(yangModuleInfo.getName().getLocalName() + ".yang"),
-                        sanitizedYangByteSource));
+                        sanitizedYangByteSource, Charset.defaultCharset()));
                 sourceArrayList.add(statementSource);
             } catch (IOException | YangParserException e) {
                 final String errorMsg = String.format("Failed to create YangStatementStreamSource from"
