77b5bc139d7bde8111a66a52cf4582e82a2080a0
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-07-26 21:05:41 +0200

    Get Identifier import from common directory
    https://github.com/opendaylight/yangtools/commit/61dd54d0ca65bb0f2ad7cd9e1fc2d30856f0e1c1
    
    JIRA: LIGHTY-235
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/identifier/IdentifierUtils.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/identifier/IdentifierUtils.java
@@ -17,7 +17,7 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
-import org.opendaylight.yangtools.yang.binding.Identifier;
+import org.opendaylight.yangtools.concepts.Identifier;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
