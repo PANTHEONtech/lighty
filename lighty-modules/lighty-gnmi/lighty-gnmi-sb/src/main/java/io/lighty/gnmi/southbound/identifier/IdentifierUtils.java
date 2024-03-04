336401cae859152c643be752c3aeaa48878c4e07
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-03-04 15:40:08 +0100

    Refactor PathArgument to DataObjectStep
    https://github.com/opendaylight/mdsal/commit/889d6606afceea88af3884ee340008c0f8810496
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/identifier/IdentifierUtils.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/identifier/IdentifierUtils.java
@@ -16,9 +16,9 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yangtools.yang.binding.DataObjectStep;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.IdentifiableItem;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.binding.KeyStep;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
@@ -46,10 +46,10 @@ public final class IdentifierUtils {
                 .build();
     }
 
-    public static NodeId nodeIdOfPathArgument(final PathArgument pathArgument)
+    public static NodeId nodeIdOfPathArgument(final DataObjectStep pathArgument)
             throws IllegalStateException {
-        if (pathArgument instanceof IdentifiableItem<?, ?> identifiableItem) {
-            final var key = identifiableItem.getKey();
+        if (pathArgument instanceof KeyStep<?, ?> identifiableItem) {
+            final var key = identifiableItem.key();
             if (key instanceof NodeKey nodeKey) {
                 return nodeKey.getNodeId();
             }
