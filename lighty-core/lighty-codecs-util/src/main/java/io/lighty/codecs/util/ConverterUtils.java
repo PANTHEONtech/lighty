d722b5b423b3bb6be129b8941a25bb2c4ad1b03f
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-04-09 18:11:57 +0200

    Clean up XmlElement methods
    https://github.com/opendaylight/netconf/commit/7702c45bbff12f5b6c21c710a1106ee6cf2991a9
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-core/lighty-codecs-util/src/main/java/io/lighty/codecs/util/ConverterUtils.java
+++ lighty-core/lighty-codecs-util/src/main/java/io/lighty/codecs/util/ConverterUtils.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.netconf.api.DocumentedException;
+import org.opendaylight.netconf.api.xml.MissingNameSpaceException;
 import org.opendaylight.netconf.api.xml.XmlElement;
 import org.opendaylight.netconf.api.xml.XmlUtil;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -90,15 +91,20 @@ public final class ConverterUtils {
      * @return {@link QName} for input data or empty.
      */
     public static Optional<QName> getRpcQName(final XmlElement xmlElement) {
-        Optional<String> optionalNamespace = xmlElement.findNamespaceAttribute();
+        String optionalNamespace = null;
+        try {
+            optionalNamespace = xmlElement.getNamespace();
+        } catch (MissingNameSpaceException e) {
+            throw new RuntimeException(e);
+        }
         String name = xmlElement.getName();
         if (Strings.isNullOrEmpty(name)) {
             return Optional.empty();
         }
         String revision = null;
         String namespace;
-        if (optionalNamespace.isPresent() && !Strings.isNullOrEmpty(optionalNamespace.get())) {
-            String[] split = optionalNamespace.get().split("\\?");
+        if (optionalNamespace != null) {
+            String[] split = optionalNamespace.split("\\?");
             if (split.length > 1 && split[1].contains("revision=")) {
                 revision = split[1].replace("revision=", "");
 
