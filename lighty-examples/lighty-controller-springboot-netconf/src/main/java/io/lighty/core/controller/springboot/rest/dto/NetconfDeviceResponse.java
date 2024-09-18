ebbf592a46f6cf8b3e27de93be4d5ea4a912e14e
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-09-18 10:22:55 +0200

    Use newer netconf-node-topology and odl-netconf-device
    https://github.com/opendaylight/netconf/commit/20a6012594a3109c9e7083864d5548cda5305c38#diff-28799befb2681209d93c0cccbad076367bcd2b93f645d48c372e7da29b7b8c17
    
    JIRA: LIGHTY-317
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/rest/dto/NetconfDeviceResponse.java
+++ lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/rest/dto/NetconfDeviceResponse.java
@@ -10,8 +10,8 @@ package io.lighty.core.controller.springboot.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev240120.ConnectionOper.ConnectionStatus;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev231121.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev240611.ConnectionOper.ConnectionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev240611.NetconfNode;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 
 public class NetconfDeviceResponse {
