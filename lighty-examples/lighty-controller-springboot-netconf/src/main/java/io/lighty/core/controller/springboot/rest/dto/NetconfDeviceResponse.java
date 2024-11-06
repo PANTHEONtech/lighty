94829df9e2a459621c9a9a6499d30efe20e007ae
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-11-06 12:38:18 +0100

    Use newer revision of odl-netconf-device
    Add range restriction for backoff-multiplier:
    https://github.com/opendaylight/netconf/commit/6b80ae0cf4716f907e784467fa772f894230373e#diff-66ffe71b2608193263cdcf5b4baf22a9166ee7ed93161637caffef6a3fba7f37
    
    JIRA: LIGHTY-339
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    (cherry picked from commit f7471657998b2166a568a884fe52c720743f204d)

--- lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/rest/dto/NetconfDeviceResponse.java
+++ lighty-examples/lighty-controller-springboot-netconf/src/main/java/io/lighty/core/controller/springboot/rest/dto/NetconfDeviceResponse.java
@@ -10,7 +10,7 @@ package io.lighty.core.controller.springboot.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev240611.ConnectionOper.ConnectionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.device.rev241009.ConnectionOper.ConnectionStatus;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev240911.NetconfNodeAugment;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 
