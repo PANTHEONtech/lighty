317b1c31e904a755840fbe3ac6947cc8e15aac31
tobias.pobocik <tobias.pobocik@pantheon.tech>
2025-03-04 13:24:57 +0100

    Remove TransactionRateLimiter
    https://github.com/opendaylight/controller/commit/e87b039734315360495dab29499973f5d815e517#diff-5624a113bf7b93c0667c799a270fbd892f1c62a63fb6f3ddea857db88a8ea505
    
    JIRA: LIGHTY-354
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/util/ControllerConfigUtils.java
+++ lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/util/ControllerConfigUtils.java
@@ -54,7 +54,7 @@ public final class ControllerConfigUtils {
         org.opendaylight.yang.svc.v1.urn.opendaylight.yang.extension.yang.ext.rev130709
                 .YangModuleInfoImpl.getInstance(),
         org.opendaylight.yang.svc.v1.urn.opendaylight.params.xml.ns.yang.controller.config.distributed.datastore
-                .provider.rev231229
+                .provider.rev250130
                 .YangModuleInfoImpl.getInstance(),
         org.opendaylight.yang.svc.v1.urn.opendaylight.params.xml.ns.yang.controller.entity.owners.norev
                 .YangModuleInfoImpl.getInstance(),
