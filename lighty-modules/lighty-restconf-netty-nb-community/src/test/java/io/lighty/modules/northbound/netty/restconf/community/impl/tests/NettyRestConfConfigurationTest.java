36c53c1d8ff7d790a485c30b2b95f14e1b4327f0
tobias.pobocik <tobias.pobocik@pantheon.tech>
2025-10-13 17:45:44 +0200

    Add NettyRestConf Tests
    Add tests based of CommunityRestConf tests to properly verify netty
    restconf and its configuration.
    
    JIRA: LIGHTY-333
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ lighty-modules/lighty-restconf-netty-nb-community/src/test/java/io/lighty/modules/northbound/netty/restconf/community/impl/tests/NettyRestConfConfigurationTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
+ */
+
+package io.lighty.modules.northbound.netty.restconf.community.impl.tests;
+
+import io.lighty.core.controller.impl.config.ConfigurationException;
+import io.lighty.modules.northbound.netty.restconf.community.impl.config.NettyRestConfConfiguration;
+import io.lighty.modules.northbound.netty.restconf.community.impl.util.NettyRestConfUtils;
+import java.net.InetAddress;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NettyRestConfConfigurationTest {
+
+    @Test
+    public void testNettyRestConfConfiguration() {
+        final NettyRestConfConfiguration defaultRestConfConfiguration =
+            NettyRestConfUtils.getDefaultNettyRestConfConfiguration();
+        final NettyRestConfConfiguration restConfConfiguration =
+            new NettyRestConfConfiguration(defaultRestConfConfiguration);
+
+        Assert.assertEquals(defaultRestConfConfiguration, restConfConfiguration);
+        Assert.assertEquals(restConfConfiguration.hashCode(), defaultRestConfConfiguration.hashCode());
+
+        restConfConfiguration.setHttpPort(3333);
+        restConfConfiguration.setInetAddress(InetAddress.getLoopbackAddress());
+
+        Assert.assertNotEquals(defaultRestConfConfiguration, restConfConfiguration);
+        Assert.assertNotEquals(restConfConfiguration.hashCode(), defaultRestConfConfiguration.hashCode());
+    }
+
+    @Test
+    public void testNettyRestConfConfigurationUtilsLoadFromStream() throws ConfigurationException {
+        final var resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("restconf-config.json");
+        final var restConfConfiguration = NettyRestConfUtils.getNettyRestConfConfiguration(resourceAsStream);
+        Assert.assertNotNull(restConfConfiguration);
+        Assert.assertEquals(restConfConfiguration.getHttpPort(), 5555);
+        Assert.assertEquals(restConfConfiguration.getInetAddress().getHostAddress(), "127.0.0.3");
+    }
+
+}
