a7e4a97aad4b913fdc323aab8d68030b13c0a5b9
tobias.pobocik <tobias.pobocik@pantheon.tech>
2025-10-24 13:56:08 +0200

    Convert GnmiSouthboundModule to osgi component
    Convert GnmiSouthboundModule as osgi component
    to allow it to run using karaf.
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-modules/lighty-gnmi/lighty-gnmi-test/src/test/java/io/lighty/modules/gnmi/test/gnmi/GnmiWithoutRestconfTest.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-test/src/test/java/io/lighty/modules/gnmi/test/gnmi/GnmiWithoutRestconfTest.java
@@ -38,7 +38,6 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import javax.crypto.SecretKey;
