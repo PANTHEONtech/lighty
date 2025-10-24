546a9cb9a412da519e17d577c8fd2d124a00a9ba
tobias.pobocik <tobias.pobocik@pantheon.tech>
2025-10-24 13:00:37 +0200

    Use openconfig models as initial
    GnmiSouthboundProvider expected to be provided a path
    to initial models from json configuration. Since we
    are no longer relying on json configuration, we need
    to provide them manually.

--- lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/lightymodule/util/GnmiConfigUtils.java
+++ lighty-modules/lighty-gnmi/lighty-gnmi-sb/src/main/java/io/lighty/gnmi/southbound/lightymodule/util/GnmiConfigUtils.java
@@ -40,6 +40,38 @@ public final class GnmiConfigUtils {
                     .YangModuleInfoImpl.getInstance()
     );
 
+    public static final Set<YangModuleInfo> OPENCONFIG_YANG_MODELS = Set.of(
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.aaa.rev200730.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.aaa.types.rev181121.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.alarms.rev190709.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.alarms.types.rev181121.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.interfaces.aggregate.rev200501.YangModuleInfoImpl
+            .getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.interfaces.ethernet.rev210609.YangModuleInfoImpl
+            .getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.interfaces.rev210406.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.license.rev200422.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.messages.rev180813.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.openconfig._if.types.rev181121.YangModuleInfoImpl
+            .getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.openconfig.ext.rev200616.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.openconfig.types.rev190416.YangModuleInfoImpl
+            .getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.openflow.rev181121.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.openflow.types.rev200630.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.platform.rev210118.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.platform.types.rev210118.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.system.logging.rev181121.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.system.procmon.rev190315.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.system.rev200413.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.system.terminal.rev181121.YangModuleInfoImpl
+            .getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.types.inet.rev210107.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.types.yang.rev210302.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.vlan.rev190416.YangModuleInfoImpl.getInstance(),
+        org.opendaylight.yang.svc.v1.http.openconfig.net.yang.vlan.types.rev200630.YangModuleInfoImpl.getInstance()
+    );
+
     private GnmiConfigUtils() {
         //Utility class
     }
