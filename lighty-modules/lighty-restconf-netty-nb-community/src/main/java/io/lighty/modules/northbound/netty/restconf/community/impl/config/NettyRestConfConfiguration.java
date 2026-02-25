0b2056100f05e4a4970164b946620d04103f3e59
tobias.pobocik <tobias.pobocik@pantheon.tech>
2026-02-25 12:46:33 +0100

    Make BootstrapFactory configurable
    Add the ability to configure groupName and workThreads using JSON
    config.
    
    JIRA: LIGHTY-398
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit 56339b1914a2ca535c50766918891e086781498c)

--- lighty-modules/lighty-restconf-netty-nb-community/src/main/java/io/lighty/modules/northbound/netty/restconf/community/impl/config/NettyRestConfConfiguration.java
+++ lighty-modules/lighty-restconf-netty-nb-community/src/main/java/io/lighty/modules/northbound/netty/restconf/community/impl/config/NettyRestConfConfiguration.java
@@ -35,6 +35,8 @@ public class NettyRestConfConfiguration {
     private InetAddress inetAddress = InetAddress.getLoopbackAddress();
     private int httpPort = 8888;
     private String restconfServletContextPath = "restconf";
+    private String groupName = "lighty-restconf-nb-worker";
+    private int workThreads = 0;
 
     public NettyRestConfConfiguration() {
     }
@@ -49,6 +51,7 @@ public class NettyRestConfConfiguration {
         this.domActionService = restConfConfiguration.getDomActionService();
         this.domNotificationService = restConfConfiguration.getDomNotificationService();
         this.domMountPointService = restConfConfiguration.getDomMountPointService();
+        this.workThreads = restConfConfiguration.getWorkThreads();
     }
 
     public NettyRestConfConfiguration(final DOMDataBroker domDataBroker, final DOMSchemaService schemaService,
@@ -60,6 +63,25 @@ public class NettyRestConfConfiguration {
         this.domActionService = domActionService;
         this.domNotificationService = domNotificationService;
         this.domMountPointService = domMountPointService;
+    }
+
+    public int getWorkThreads() {
+        return workThreads;
+    }
+
+    public final void setWorkThreads(int workThreads) {
+        if (workThreads < 0) {
+            throw new IllegalArgumentException("workThreads cannot be negative");
+        }
+        this.workThreads = workThreads;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public final void setGroupName(String groupName) {
+        this.groupName = groupName;
     }
 
     public InetAddress getInetAddress() {
@@ -147,13 +169,15 @@ public class NettyRestConfConfiguration {
             && Objects.equals(domNotificationService, that.domNotificationService)
             && Objects.equals(domMountPointService, that.domMountPointService)
             && Objects.equals(inetAddress, that.inetAddress)
-            && Objects.equals(restconfServletContextPath, that.restconfServletContextPath);
+            && Objects.equals(restconfServletContextPath, that.restconfServletContextPath)
+            && Objects.equals(groupName, that.groupName)
+            && Objects.equals(workThreads, that.workThreads);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(domDataBroker, schemaService, domRpcService, domActionService, domNotificationService,
-            domMountPointService, inetAddress, httpPort, restconfServletContextPath);
+            domMountPointService, inetAddress, httpPort, restconfServletContextPath, groupName, workThreads);
     }
 
 }
