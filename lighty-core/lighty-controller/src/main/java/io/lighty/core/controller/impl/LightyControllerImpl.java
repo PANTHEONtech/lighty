c07c181b7a8892ff6535fedc6ec737dc4eddab2b
tobias.pobocik <tobias.pobocik@pantheon.tech>
2026-06-30 13:03:19 +0200

    Support sal-remote:create-notification-stream
    Add support for create-notification-stream by adding the missing RPC implementation
    and fixing the strict schema validation in Lighty.io which enables all Yang features
    by default. This inadvertently enabled the replay feature in
    ietf-subscribed-notifications, making the replay-log-creation-time leaf strictly
    mandatory.
    
    JIRA: LIGHTY-440
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>
    (cherry picked from commit 7c247ed1883382b74322db1a2b38513f012bdbca)

--- lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/LightyControllerImpl.java
+++ lighty-core/lighty-controller/src/main/java/io/lighty/core/controller/impl/LightyControllerImpl.java
@@ -32,6 +32,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -110,6 +111,7 @@ import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSe
 import org.opendaylight.yangtools.binding.data.codec.impl.BindingCodecContext;
 import org.opendaylight.yangtools.binding.data.codec.impl.di.DefaultDynamicBindingDataCodec;
 import org.opendaylight.yangtools.binding.generator.impl.DefaultBindingRuntimeGenerator;
+import org.opendaylight.yangtools.binding.meta.YangFeatureProvider;
 import org.opendaylight.yangtools.binding.meta.YangModuleInfo;
 import org.opendaylight.yangtools.binding.runtime.api.BindingRuntimeGenerator;
 import org.opendaylight.yangtools.binding.runtime.api.BindingRuntimeTypes;
@@ -265,6 +267,12 @@ public class LightyControllerImpl extends AbstractLightyModule implements Lighty
 
         //INIT schema context
         this.snapshotResolver = new ModuleInfoSnapshotResolver("binding-dom-codec", yangParserFactory);
+        for (YangFeatureProvider featureProvider : ServiceLoader.load(YangFeatureProvider.class)) {
+            this.snapshotResolver.registerModuleFeatures(
+                featureProvider.boundModule(),
+                featureProvider.supportedFeatures()
+            );
+        }
         this.modelsRegistration = snapshotResolver.registerModuleInfos(modelSet);
         this.moduleInfoSnapshot = snapshotResolver.takeSnapshot();
         this.schemaService = new FixedDOMSchemaService(() -> moduleInfoSnapshot.modelContext(), sourceId -> {
